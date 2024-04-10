b9e5dd7d39b29a5fb8bdedd49da56240b7e7ce42
Sandesh Bhujbal <92014891+sandesh300@users.noreply.github.com>
2024-04-10 19:31:57 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,23 +1,17 @@
 public class ReverseString {
-    public static String reverseString(String input) {
-        // Check for null or empty string
-        if (input == null || input.isEmpty()) {
-            return input;
-        }
-
+    
+       static String reverseStr(String str) 
+       {
         StringBuilder reversed = new StringBuilder();
-
-        // Iterate through the input string in reverse order and append characters to the StringBuilder
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed.append(input.charAt(i));
+        for (int i = str.length() - 1; i >= 0; i--) 
+        {
+            reversed.append(str.charAt(i));
         }
-
         return reversed.toString();
     }
-
-    public static void main(String[] args) {
-        String inputString = "hello";
-        String reversedString = reverseString(inputString);
-        System.out.println("Reversed string: " + reversedString);
+    public static void main(String[] args) 
+    {
+        String original = "Hello, World!";
+	System.out.println("Reversed String is = " + reverseStr(original));
     }
 }
